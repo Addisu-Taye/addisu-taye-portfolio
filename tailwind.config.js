fe6f8e17bfdfffafc2921272b5a1ecdbb4c399fc
Addisu-Taye <addtaye@gmai.com>
2025-06-22 12:16:57 +0300

    22-JUN-2025 1

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // Enables dark mode via class strategy
+  theme: {
+    extend: {
+      animation: {
+        typing: 'typing 4s steps(40, end) forwards',
+        blink: 'blink 0.75s step-end infinite',
+      },
+      keyframes: {
+        typing: {
+          '0%': { width: '0' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
